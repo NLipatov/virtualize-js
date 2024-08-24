c572e04fd728303e0c375e25d4927448ff28e805
Nikita <6stringsohei@gmail.com>
2024-08-24 19:05:10 +0300

    moved files to docs folder for gh pages

---
+++ docs/demo.js
@@ -0,0 +1,54 @@
+'use strict';
+import { render } from '../src/virtualize.js';
+
+const itemsContainer = document.getElementById('items-container');
+const renderButton = document.getElementById('renderButton');
+const itemCountInput = document.getElementById('itemCount');
+
+const colors = ['#f0f0f0', '#d0e8ff', '#d0ffd0', '#fffacd', '#ffd0d0'];
+
+let items = [];
+
+function generateItems(count) {
+    items = [];
+
+    for (let i = 0; i < count; i++) {
+        const height = getRandomHeight(50, 300);
+        const colorIndex = i % colors.length;
+        items.push({
+            index: i,
+            text: `Item #${i + 1}`,
+            height,
+            color: colors[colorIndex],
+        });
+    }
+
+    render(items, itemsContainer);
+}
+
+const getRandomHeight = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+renderButton.addEventListener('click', function () {
+    const count = parseInt(itemCountInput.value, 10);
+
+    if (count > 0) {
+        generateItems(count);
+        itemsContainer.style.height = `${window.innerHeight - document.querySelector('footer').offsetHeight}px`;
+        itemsContainer.style.overflowY = 'scroll';
+        render(items, itemsContainer);
+    } else {
+        alert('Invalid item count!');
+    }
+});
+
+
+itemsContainer.addEventListener('scroll', () => {
+    render(items, itemsContainer);
+});
+
+window.addEventListener('resize', () => {
+    itemsContainer.style.height = `${window.innerHeight - document.querySelector('footer').offsetHeight}px`;
+    render(items, itemsContainer);
+});

2b338099df42723c072f20a671a32a8f784e3dba
Nikita <6stringsohei@gmail.com>
2024-08-25 19:30:14 +0300

    webpack.config.js update

--- gh-pages-dist/webpack.config.js
+++ gh-pages-dist/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './demo.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../docs'),
+    path: path.resolve(__dirname, './docs'),
   },
   module: {
     rules: [
